5f0bc3b82d53d8263e8de82ca0774179f86ceebf
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2025-01-02 22:25:26 +0600

    Global error handler function is added

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,35 @@
 import app from './app/app';
 import colors from 'colors';
 import configInfo from './app/config';
+import mongoose from 'mongoose';
+import { NextFunction, Request, Response } from 'express';
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+async function connectDB() {
+  try {
+    await mongoose.connect(configInfo.db_url as string, {
+      dbName: 'car_store_DB',
+    });
+    await console.log(
+      colors.green('Server connected with database successfully...'),
+    );
+  } catch (error: any) {
+    console.log(colors.red('server failed to connect db.'));
+  }
+}
+
+connectDB();
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Your request url [${req.url}] is not valid.`);
+  next(error);
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: error.message });
 });
 
 app.listen(configInfo.port, () => {
