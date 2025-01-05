aae6df703448ed807892b5a5f8ad98e92c6ebaaa
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2025-01-05 14:48:45 +0600

    Total revenue is calculated.

--- src/modules/revenue/revenue.controller.ts
+++ src/modules/revenue/revenue.controller.ts
@@ -2,9 +2,23 @@ import { Request, Response } from 'express';
 import { Order } from '../order/corder.model';
 
 const getAllRevenue = async (req: Request, res: Response) => {
-  const totalRevenue = await Order.aggregate([{ $match: {} }]);
-  console.log(totalRevenue);
-  res.json(totalRevenue);
+  // Total revenue calculated from all orders
+  const totalRevenue = await Order.aggregate([
+    {
+      $addFields: { netPrice: { $multiply: ['$quantity', '$totalPrice'] } },
+    },
+    {
+      $group: { _id: null, totalRevenue: { $sum: '$netPrice' } },
+    },
+  ]);
+
+  res.json({
+    message: 'Revenue calculated successfully',
+    status: true,
+    data: {
+      totalRevenue: totalRevenue[0]?.totalRevenue,
+    },
+  });
 };
 
 const revenueController = {
